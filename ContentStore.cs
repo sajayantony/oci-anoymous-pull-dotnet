3c9fd8291acde3cc178b9ced4dd37f172cfb69e0
Sajay Antony <sajaya@microsoft.com>
2020-03-11 17:34:23 -0700

    Support multiple layer download

--- ContentStore.cs
+++ ContentStore.cs
@@ -12,31 +12,36 @@ using System.IO;
 namespace OCIArtifact.Samples
 {
     public class ContentStore
-    {        
-        public static async Task  Pull(string registry, string repo,string tag)
+    {
+        public static async Task Pull(string registry, string repo, string tag)
         {
-            var image = new ImageRef(){
-                        HostName = registry,
-                        Repository = repo,
-                        Tag = tag
-                    };
+            var image = new ImageRef()
+            {
+                HostName = registry,
+                Repository = repo,
+                Tag = tag
+            };
             var loginUri = $"https://{registry}";
             AzureContainerRegistryClient runtimeClient = new AzureContainerRegistryClient(registry, new AnonymousToken(image));
 
             //Get manifest 
             var manifestResponse = runtimeClient.Manifests.GetAsync(repo, tag, "application/vnd.oci.image.manifest.v1+json").Result;
             Console.WriteLine("Manifest:");
-            Console.WriteLine(JsonSerializer.Serialize(manifestResponse,new JsonSerializerOptions(){WriteIndented = true}).ToString());
+            Console.WriteLine(JsonSerializer.Serialize(manifestResponse, new JsonSerializerOptions() { WriteIndented = true }).ToString());
 
-            //Dowload multiple layers here. 
-            var l0 = manifestResponse.Layers[0];
-            var blobStream = await runtimeClient.Blob.GetAsync(repo, l0.Digest);
-            var fileName = l0.Annotations.Title;
-            Console.WriteLine($"Writing File: {fileName}");
-            using(FileStream fs = File.OpenWrite(fileName))
+            //Dowload multiple layers here.
+            Console.WriteLine("Starting Layer Download.....");
+            for (int i = 0; i < manifestResponse.Layers.Count; i++)
             {
-                await blobStream.CopyToAsync(fs);
-            }   
+                var l0 = manifestResponse.Layers[0];
+                var blobStream = await runtimeClient.Blob.GetAsync(repo, l0.Digest);
+                var fileName = l0.Annotations.Title;
+                Console.WriteLine($"Writing File: {fileName}");
+                using (FileStream fs = File.OpenWrite(fileName))
+                {
+                    await blobStream.CopyToAsync(fs);
+                }
+            }
         }
 
         class AnonymousToken : ServiceClientCredentials
@@ -45,7 +50,7 @@ namespace OCIArtifact.Samples
 
             public AnonymousToken(ImageRef image)
             {
-                _image = image;   
+                _image = image;
             }
 
             public override void InitializeServiceClient<T>(ServiceClient<T> client)
@@ -56,15 +61,15 @@ namespace OCIArtifact.Samples
             private async Task<string> GetAccessToken()
             {
                 HttpClient c = new HttpClient();
-            
+
                 var service = _image.HostName;
                 var repo = _image.Repository;
-                var hostname  = System.Environment.MachineName;
+                var hostname = System.Environment.MachineName;
                 var scope = $"repository:{repo}:pull";
                 string uri = $"https://{service}/oauth2/token?client={hostname}&scope={scope}&service={service}";
 
                 var response = c.GetAsync(uri).Result;
-                var strResponse  = await response.Content.ReadAsStringAsync();
+                var strResponse = await response.Content.ReadAsStringAsync();
 
                 //Console.WriteLine(strResponse);
                 var jToken = JsonSerializer.Deserialize<AuthToken>(strResponse);
@@ -81,14 +86,14 @@ namespace OCIArtifact.Samples
 
         class AuthToken
         {
-            public string access_token {get;set;}
+            public string access_token { get; set; }
         }
 
         public class ImageRef
         {
-            public string HostName {get;set;}
-            public string Repository {get;set;}
-            public string Tag {get; set; }
+            public string HostName { get; set; }
+            public string Repository { get; set; }
+            public string Tag { get; set; }
         }
     }
 }
